e1a8e3ecfc01a0ab4f1818757f93023adbe2fa87
Bappy60 <kawsar.ahmed.bappy.148@gmail.com>
2023-04-06 17:19:42 +0600

    fixed some validation issue

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
--- pkg/types/structs.go
+++ pkg/types/structs.go
@@ -4,24 +4,24 @@ import (
 	"errors"
 	"regexp"
 
-	validation "github.com/go-ozzo/ozzo-validation"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/go-ozzo/ozzo-validation/is"
 )
 
-type BookReqStruc struct{
-	ID              string   
-	Name            string 
-	PublicationYear string    
-	NumberOfPages   string    
-	AuthorID        string  
-	Publication     string 
+type BookReqStruc struct {
+	ID              string
+	Name            string
+	PublicationYear string
+	NumberOfPages   string
+	AuthorID        string
+	Publication     string
 }
 
 type AuthorReqStruc struct {
-	ID    string 
-	Name  string 
-	Email string 
-	Age   string    
+	ID    string
+	Name  string
+	Email string
+	Age   string
 }
 
 type FilterBookStruc struct {
@@ -33,10 +33,10 @@ type FilterBookStruc struct {
 	Publication     *string
 }
 
-type FilterAuthorStruc struct{
-	ID    uint 
-	Name  string 
-	Email string 
+type FilterAuthorStruc struct {
+	ID    uint
+	Name  string
+	Email string
 	Age   int64
 }
 
@@ -47,6 +47,7 @@ type CreateBookStruc struct {
 	AuthorID        uint   `gorm:"index" json:"author_id"` // foreign key
 	Publication     string `json:"publication"`
 }
+
 func (book CreateBookStruc) Validate() error {
 	return validation.ValidateStruct(&book,
 		validation.Field(&book.Name, validation.Required, validation.Length(3, 50)),
@@ -71,18 +72,6 @@ func (createAuthorStruc CreateAuthorStruc) Validate() error {
 	)
 }
 
-func AuthorNameValidator (value interface{}) error{
-	if str, ok := value.(string); ok {
-		match, err := regexp.MatchString("^[A-Za-z ]+$", str)
-		if err != nil {
-			return err
-		}
-		if !match {
-			return  errors.New("author name should contain only letters and spaces")
-		}
-	}
-	return nil
-}
 type UpdateBookStruc struct {
 	ID              string
 	Name            string `json:"name"`
@@ -102,15 +91,28 @@ func (updateBookStruc UpdateBookStruc) Validate() error {
 
 type UpdateAuthorStruc struct {
 	ID    string
-	Name  string
-	Email string
-	Age   int
+	Name  string `json:"name,omitempty"`
+	Email string `json:"email,omitempty"`
+	Age   int    `json:"age,omitempty"`
 }
 
 func (updateAuthorStruc UpdateAuthorStruc) Validate() error {
 	return validation.ValidateStruct(&updateAuthorStruc,
-		validation.Field(&updateAuthorStruc.Name, validation.Length(3, 50)),
+		validation.Field(&updateAuthorStruc.Name, validation.When(updateAuthorStruc.Name != "", validation.By(AuthorNameValidator)), validation.Length(3, 50)),
 		validation.Field(&updateAuthorStruc.Email, is.Email),
-		validation.Field(&updateAuthorStruc.Age, validation.Min(5), validation.Max(150)),
-	)
+
+		validation.Field(&updateAuthorStruc.Age, validation.Min(int(0)).Exclusive(), validation.NilOrNotEmpty.Error("must be greater than 0"), validation.Max(150)))
+}
+
+func AuthorNameValidator(value interface{}) error {
+	if str, ok := value.(string); ok {
+		match, err := regexp.MatchString("^[A-Za-z ]+$", str)
+		if err != nil {
+			return err
+		}
+		if !match {
+			return errors.New("author name should contain only letters and spaces")
+		}
+	}
+	return nil
 }
